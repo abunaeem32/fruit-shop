ccf978ebd3bf9b1d21f7918c79ebf9377d4e58ca
abunaeem32 <mortensberg32@gmail.com>
2023-07-27 22:31:28 +0600

    100% Ready Without Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'team-bg': "url('./images/team.png')",
+          
+        }
+      },
     },
     plugins: [],
   }
